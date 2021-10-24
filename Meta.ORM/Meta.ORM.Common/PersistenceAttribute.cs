973efba69283e1f2a062c2d5e03b1cca58c337d1
Alexander Nesvizhsky <alexander.nesvizhsky@acumatica.com>
2021-10-24 13:02:15 +0300

    Some base entities and attributes added

---
+++ Meta.ORM/Meta.ORM.Common/PersistenceAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Meta.ORM.Common
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false, Inherited = true)]
+    public abstract class PersistenceAttribute : Attribute
+    {
+    }
+
+    public class RelationalPersistenceAttribute : PersistenceAttribute
+    {
+        
+    }
+
+    public class UnstructuredPersistenceAttribute : PersistenceAttribute
+    {
+        
+    }
+
+    public class InMemoryPersistenceAttribute : Attribute
+    {
+        
+    }
+}
